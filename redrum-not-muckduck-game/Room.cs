3a43908dfb9640cf64e1651676960a68aeacf56e
Evan Slaton <evanslaton@gmail.com>
2020-02-17 23:37:18 -0800

    Removed unecessary comments

--- redrum-not-muckduck-game/Room.cs
+++ redrum-not-muckduck-game/Room.cs
@@ -2,8 +2,6 @@
 
 namespace redrum_not_muckduck_game
 {
-    // This class creates new rooms for the game
-    // Each room has a name, description, item, person, & adjacent rooms. 
     public class Room
     {
         public string Name { get; set; }
